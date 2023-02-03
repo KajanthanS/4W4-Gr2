33cc72d1360f6284084e89032da31bae61657248
Kajanthan Seevarathinam <seevarathinamkajanthan@gmail.com>
2023-02-03 11:16:10 -0500

    lab2 ajout couleurs et menus

--- wp-content/themes/4W4-Kajanthan/functions.php
+++ wp-content/themes/4W4-Kajanthan/functions.php
@@ -1,25 +1,22 @@
-<?php 
-
+<?php
 
 function ajouter_styles() {
-        wp_enqueue_style( 'style-principale', // Identifiant du link css
-         get_template_directory_uri() . '/css/style.css', // Endroit où se trouve le fichier style.css
-         array(), // Les fichiers css qui dépendent de style.css
-         fileatime(get_template_directory() . "/style.css"), // version de notre style.css
-        );
+
+wp_enqueue_style(   'style-principale',  // identificateur du link css
+                    get_template_directory_uri() . '/style.css',  // enroit où se trouve le fichier style.css
+                    array(), // les fichiers css qui dépendent de style.css
+                    filemtime(get_template_directory() . '/style.css')  // version de notre style.css
+); 
 }
-        add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
+add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
 
 
-// Enregistrement des menus
 
-    function enregistrement_nav_menu(){
-        register_nav_menus( array(
-            'principal' => 'Menu principal',
-            'footer'  => 'Menu pied de page',
-        ) );
-    }
-    add_action( 'after_setup_theme', 'enregistrement_nav_menu', 0 );
-
-
-?>
+/* ----------------------------------- Enregistrement des menus */
+function enregistrement_nav_menu(){
+    register_nav_menus( array(
+        'principal' => 'Menu principal',
+        'footer'  => 'Menu pied de page'
+    ) );
+}
+add_action( 'after_setup_theme', 'enregistrement_nav_menu', 0 );  
