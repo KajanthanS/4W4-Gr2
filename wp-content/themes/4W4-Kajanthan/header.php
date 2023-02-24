71e0ee77ea71d6b2820c4e2acba7ddc380e285bb
Kajanthan Seevarathinam <seevarathinamkajanthan@gmail.com>
2023-02-24 10:45:42 -0500

    Modifications du logo et ajout des requêtes médias

--- wp-content/themes/4W4-Kajanthan/header.php
+++ wp-content/themes/4W4-Kajanthan/header.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
     <?php wp_head(); ?>
 </head>
 
@@ -15,8 +18,9 @@
          <?php wp_nav_menu(array(
             "menu" => "entete",
             "container" => "nav"
-    )) ?>
-    </div>
+            )) ?>
+        </div>
+        <h3 class="icon-burger"><span class="material-symbols-outlined">menu</span></h3>
     <?php get_search_form() ?>
     </section>
     <h1><a href="<?= bloginfo('url') ?>"><?= bloginfo('name') ?></a></h1>
