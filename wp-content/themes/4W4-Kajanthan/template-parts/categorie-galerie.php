12fced5fec6e15ad48f86fdcc3ca181098d72637
KajanthanS <e2124480@cmaisonneuve.qc.ca>
2023-04-14 11:35:27 -0400

    Carrousel ACF

--- wp-content/themes/4W4-Kajanthan/template-parts/categorie-galerie.php
+++ wp-content/themes/4W4-Kajanthan/template-parts/categorie-galerie.php
@@ -5,6 +5,9 @@
  
  <article class="blocflex__galerie">
     <?php the_content(); ?>
+    <?php the_field('couleur') ?>
+    <p><?php the_field('message') ?></p>
+    <p><?php the_field('style') ?></p>
  </article>
 
  
