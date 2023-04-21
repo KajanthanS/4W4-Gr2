c155b6170ed7c1ebefbcd4d5840bd82c4cfa18a2
KajanthanS <e2124480@cmaisonneuve.qc.ca>
2023-04-21 09:46:01 -0400

    Ouverture carrousel avec images de la galerie

--- wp-content/themes/4W4-Kajanthan/template-parts/categorie-galerie.php
+++ wp-content/themes/4W4-Kajanthan/template-parts/categorie-galerie.php
@@ -4,7 +4,8 @@
  */ -->
  
  <article class="blocflex__galerie">
-    <?php the_content(); ?>
+  
+   <?php the_content(); ?>
     <?php the_field('couleur') ?>
     <p><?php the_field('message') ?></p>
     <p><?php the_field('style') ?></p>
